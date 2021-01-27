b5f4e932ee15f1261b750489cd1cefdf2b4eda47
Alan Regan <S00188509@mail.itsligo.ie>
2021-01-27 10:06:38 +0000

    Add project files.

---
+++ Week1Lab12021/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Week1Lab12021
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Week1Lab12021/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Tracker.WebAPIClient;
+
+namespace Week1Lab12021
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ActivityAPIClient.Track(StudentID: "S00188509", StudentName: "Alan Regan", activityName: "Rad302 Week 1 Lab 1", Task: "Project Setup");
+
+        }
+    }
+}
